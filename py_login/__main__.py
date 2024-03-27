3c41a66a0f617275170a1e31b8d68d8f2b2c28fb
ShamNeko <mkdvlp01@gmail.com>
2024-03-27 21:46:16 +0900

    いったんリセット

--- py_login/__main__.py
+++ py_login/__main__.py
@@ -26,34 +26,10 @@ class App(tk.Frame):
         self.master = master # 親インスタンス
         self.pack()
         self.widgets(self) # ウィジェット配置
-        self.home = Home(master=self) # ホームインスタンス生成
 
     def widgets(self, master):
         self.txt = ttk.Label(master, text="アプリフレーム")
         self.txt.pack()
-        self.del_button = ttk.Button(master, text="アプリフレーム削除", command=self.del_process)
-        self.del_button.pack()
-    
-    def del_process(self):
-        self.destroy()
-"""
-ホーム画面クラス
-"""
-class Home(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master, bg="skyblue")
-        self.master = master # 親インスタンス
-        self.pack()
-        self.widgets(master) # ウィジェット配置
-
-    def widgets(self, master):
-        self.txt = ttk.Label(master, text="ホーム画面フレーム")
-        self.txt.pack()
-        self.del_button = ttk.Button(master, text="ホームフレーム削除", command=self.del_process)
-        self.del_button.pack()
-        
-    def del_process(self):
-        self.destroy()
 
 if __name__ == "__main__":
     main()
